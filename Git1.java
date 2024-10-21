ddfe7418348e7dda7e386bd372697c6d18a6c8b4
Bandsa <kirranbond@gmail.com>
2024-10-21 14:23:29 -0500

    first commit

---
+++ Git1.java
@@ -0,0 +1,25 @@
+package Practice;
+
+public class Git1 {
+	public void  add(){
+	 int a=10;
+	 int b=20;
+	 int sum=a+b;
+	 System.out.println("sum="+sum);
+	
+		
+	}
+
+	public static  void  main(String[] args){
+		
+		Git1 obj1= new Git1();
+		obj1.add();
+		
+		
+		
+		
+		
+
+	}
+
+}
