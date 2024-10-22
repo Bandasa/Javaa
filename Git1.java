db5578b4413055e0f51c75a2110c5c6ab76b4fa8
Bandsa <kirranbond@gmail.com>
2024-10-22 11:51:43 -0500

    second commit

--- Git1.java
+++ Git1.java
@@ -1,19 +1,25 @@
 package Practice;
 
 public class Git1 {
-	public void  add(){
-	 int a=10;
-	 int b=20;
-	 int sum=a+b;
-	 System.out.println("sum="+sum);
+	public void  add(int a , int b){
+		System.out.print("Addition of two numbers ");
+	System.out.println(a+b);
 	
-		
+		}
+	public void sub(int a,int b) {
+		System.out.print("Subtraction of two numbers ");
+		System.out.println(a-b);
+	}
+	public void mul(int a,int b) {
+		System.out.println("Multiplication of two numbers " +a*b);
 	}
 
 	public static  void  main(String[] args){
 		
 		Git1 obj1= new Git1();
-		obj1.add();
+		obj1.add(10,20);
+		obj1.sub(30, 20);
+		obj1.mul(3, 4);
 		
 		
 		
